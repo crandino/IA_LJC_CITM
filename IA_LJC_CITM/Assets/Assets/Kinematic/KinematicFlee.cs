d666615bce48078fd04cb2725f64cd918d1c0be3
Jordi Nunes <jordinunes96@gmail.com>
2016-09-23 10:01:51 +0200

    23/09 IA

---
+++ IA_LJC_CITM/Assets/Assets/Kinematic/KinematicFlee.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicFlee : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 6: To create flee just switch the direction to go
+        Vector3 direction = (move.target.transform.position - transform.position);
+        direction.x *= -1;
+        direction.z *= -1;
+        move.SetMovementVelocity(direction);
+        move.mov_velocity = move.mov_velocity.normalized * move.max_mov_velocity;
+    }
+}
---
+++ IA_LJC_CITM/Assets/Assets/Kinematic/KinematicSeek.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicSeek : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 5: Set movement velocity to max speed in the direction of the target
+        Vector3 direction = move.target.transform.position - transform.position;
+        move.SetMovementVelocity(direction);
+        move.mov_velocity = move.mov_velocity.normalized * move.max_mov_velocity;
+	}
+}
