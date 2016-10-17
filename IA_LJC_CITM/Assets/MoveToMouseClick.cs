fa9caae81b4ca39705e220caf1f3cb64c8444350
crandino <carlosmanbe@hotmail.com>
2016-10-17 09:23:25 +0200

    playing with blend animations

--- IA_LJC_CITM/Assets/MoveToMouseClick.cs
+++ IA_LJC_CITM/Assets/MoveToMouseClick.cs
@@ -3,15 +3,27 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class MoveToMouseClick : MonoBehaviour {
+
+    NavMeshAgent agent;
+    // Mejor array de GameObjects[] agents;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		if(Input.GetMouseButton(0))
 		{
 			RaycastHit hit;
 			Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if(Physics.Raycast(r, out hit) == true)
-				transform.position = hit.point;
+            {
+                //transform.position = hit.point;
+                agent.SetDestination(hit.point);
+            }				
 		}
 	}
 
