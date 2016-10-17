ad0df7ff1ca0cebd0e704ffa660301f87995f07e
crandino <carlosmanbe@hotmail.com>
2016-10-17 09:23:25 +0200

    playing with blend animations

---
+++ IA_LJC_CITM/Assets/AnimControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimControl : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    Animator anim_controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim_controller = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < agent.radius)
+            anim_controller.SetBool("run", false);
+        else
+        {
+            anim_controller.SetBool("run", true);
+            anim_controller.SetFloat("vel_x", agent.velocity.x);
+            anim_controller.SetFloat("vel_y", agent.velocity.z);
+        }            
+    }
+}
