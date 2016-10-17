452183ad24ccb7beb299cfb31e5365fcbfb509e8
crandino <carlosmanbe@hotmail.com>
2016-10-17 09:56:03 +0200

    CameraMove

---
+++ IA_LJC_CITM/Assets/Scripts/MoveToMouseClick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoveToMouseClick : MonoBehaviour {
+
+    NavMeshAgent agent;
+    // Mejor array de GameObjects[] agents;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButton(0))
+		{
+			RaycastHit hit;
+			Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(r, out hit) == true)
+            {
+                //transform.position = hit.point;
+                agent.SetDestination(hit.point);
+            }				
+		}
+	}
+
+}
--- IA_LJC_CITM/Assets/Scripts/Steering/SteeringFollowPath.cs
+++ IA_LJC_CITM/Assets/Scripts/Steering/SteeringFollowPath.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-using BansheeGz.BGSpline.Components;
+//using BansheeGz.BGSpline.Components;
 
 public class SteeringFollowPath : SteeringAbstract
 {
-    public BGCcMath bgcc_math;
+    //public BGCcMath bgcc_math;
     public float radius;
     public float ahead_percentatge; // On %
     public bool reverse;
@@ -16,23 +16,23 @@ public class SteeringFollowPath : SteeringAbstract
     void Start()
     {
         move = GetComponent<Move>();
-        incr = ahead_percentatge * bgcc_math.GetDistance() / 100.0f;       
+       // incr = ahead_percentatge * bgcc_math.GetDistance() / 100.0f;       
     }
 
     // Update is called once per frame
     void Update ()
     {
-        new_pos = bgcc_math.CalcPositionByClosestPoint(transform.position);
+        //new_pos = bgcc_math.CalcPositionByClosestPoint(transform.position);
 
         if (reverse)
             incr = -incr;       
 
         if (Vector3.Distance(new_pos, transform.position) < radius)
         {
-            float dist; bgcc_math.CalcPositionByClosestPoint(transform.position, out dist);
-            float ratio = (dist + incr) / bgcc_math.GetDistance();
-            ratio = ratio > 1.0f ? 0.0f : ratio;
-            new_pos = bgcc_math.CalcPositionByDistanceRatio(ratio);
+            //float dist; bgcc_math.CalcPositionByClosestPoint(transform.position, out dist);
+            //float ratio = (dist + incr) / bgcc_math.GetDistance();
+            //ratio = ratio > 1.0f ? 0.0f : ratio;
+            //new_pos = bgcc_math.CalcPositionByDistanceRatio(ratio);
         } 
         GetComponent<SteeringSeek>().Steer(new_pos);
     }
